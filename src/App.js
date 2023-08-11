d2133e0aa961420f6c526fe7eeae9bfccb7db9f7
Sung Yoo <yoowook1207@gmail.com>
2023-08-10 23:01:41 -0400

    npm ci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          The app is edited!
+          The app is edited!!!
         </h1>
         <a
           className="App-link"
